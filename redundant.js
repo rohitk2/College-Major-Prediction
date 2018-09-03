3458fef5f1781d60d80b570b419245101e959b62
rohitk2 <rohit98.kumar@gmail.com>
2018-09-03 00:30:00 -0700

    Add files via upload

---
+++ redundant.js
@@ -0,0 +1,48 @@
+//Student Preferences and Information         
+var divElement = document.getElementById('viz1535432013132');                    
+var vizElement = divElement.getElementsByTagName('object')[0];                    
+vizElement.style.minWidth='570px';
+vizElement.style.maxWidth='550px';
+vizElement.style.width='100%';
+vizElement.style.minHeight='687px';
+vizElement.style.maxHeight='687px';
+vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    
+var scriptElement = document.createElement('script');                   
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+vizElement.parentNode.insertBefore(scriptElement, vizElement);    
+//Student Profile
+var divElement_1 = document.getElementById('viz1535432650148');                    
+var vizElement_1 = divElement_1.getElementsByTagName('object')[0];                    
+vizElement_1.style.minWidth='570px';
+vizElement_1.style.maxWidth='550px';
+vizElement_1.style.width='100%';
+vizElement_1.style.minHeight='687px';
+vizElement_1.style.maxHeight='687px';
+vizElement_1.style.height=(divElement_1.offsetWidth*0.75)+'px';                    
+var scriptElement_1 = document.createElement('script');                    
+scriptElement_1.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_1.parentNode.insertBefore(scriptElement_1, vizElement_1);   
+//Parent Profile
+var divElement_2 = document.getElementById('viz1535433001791');                    
+var vizElement_2 = divElement_2.getElementsByTagName('object')[0];                    
+vizElement_2.style.minWidth='570px';
+vizElement_2.style.maxWidth='550px';
+vizElement_2.style.width='100%';
+vizElement_2.style.minHeight='687px';
+vizElement_2.style.maxHeight='687px';
+vizElement_2.style.height=(divElement_2.offsetWidth*0.75)+'px';                    
+var scriptElement_2 = document.createElement('script');                    
+scriptElement_2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_2.parentNode.insertBefore(scriptElement_2, vizElement_2);    
+//Information about Major
+var divElement_3 = document.getElementById('viz1535433332716');                    
+var vizElement_3= divElement_3.getElementsByTagName('object')[0];                    
+vizElement_3.style.minWidth='570px';
+vizElement_3.style.maxWidth='550px';
+vizElement_3.style.width='100%';
+vizElement_3.style.minHeight='687px';
+vizElement_3.style.maxHeight='687px';
+vizElement_3.style.height=(divElement_3.offsetWidth*0.75)+'px';                    
+var scriptElement_3 = document.createElement('script');                    
+scriptElement_3.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_3.parentNode.insertBefore(scriptElement_3, vizElement_3);    
